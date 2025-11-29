26d3503764b0104bc7c7051f75384e14af8b8159
BernieHuang <berniehuang2008@163.com>
2025-11-29 21:31:16 +0800

    x

--- backend/config.py.template
+++
@@ -1,6 +0,0 @@
-# API配置文件
-# 请根据实际情况填写相应的API密钥和配置
-
-# PaddleOCR API 配置
-PADDLE_OCR_API_URL = "https://xxx.aistudio-app.com/layout-parsing"
-PADDLE_OCR_TOKEN = "xxx"
--- backend/config.template.py
+++ backend/config.template.py
@@ -2,5 +2,5 @@
 # 请根据实际情况填写相应的API密钥和配置
 
 # PaddleOCR API 配置
-PADDLE_OCR_API_URL = "https://c8s16af3r0gd36g6.aistudio-app.com/layout-parsing"
-PADDLE_OCR_TOKEN = "7ad08cbd63f5ba8d355eb517d00a23158c419983"
+PADDLE_OCR_API_URL = "https://xxx.aistudio-app.com/layout-parsing"
+PADDLE_OCR_TOKEN = "xxx"
